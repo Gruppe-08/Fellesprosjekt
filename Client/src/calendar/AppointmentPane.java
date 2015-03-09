3cb12ed33c0a714eaa9737d4856598077665aa8b
Snikanes <eirik.vikanes@gmail.com>
2015-03-09 10:08:16 +0100

    Worked more on the dayview, one can now change dates and watch appointment for any day one wants

--- Client/src/calendar/AppointmentPane.java
+++ Client/src/calendar/AppointmentPane.java
@@ -5,8 +5,14 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 import models.Appointment;
+import javafx.geometry.Insets;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 public class AppointmentPane extends VBox {
@@ -18,43 +24,26 @@ public class AppointmentPane extends VBox {
 		this.appointment = appointment;
 		this.setPrefWidth(300);
 		this.setPrefHeight(calculateHeight());
-		this.setStyle("-fx-background-color: red");
-		this.putText();		
+		this.setPadding(new Insets(5,5,5,5));
+		this.setStyle("-fx-background-color: #4ca1ff");
+		this.putText();
+		DropShadow dropShadow = new DropShadow();
+		dropShadow.setColor(Color.web("#38597F", 1.0));
+		this.setEffect(dropShadow);
 	}
 	
 	private int calculateHeight() {
 		LocalDateTime startTime = util.DateUtil.deserialize(this.appointment.getStartTime());
 		LocalDateTime endTime = util.DateUtil.deserialize(this.appointment.getEndTime());
 		Long diff = startTime.until(endTime, ChronoUnit.MINUTES);
-		AnchorPane.setLeftAnchor(this, 50.0);
+		AnchorPane.setLeftAnchor(this, 75.0);
 		int height = (int)(0.85 * diff) + 1;
 		return height;
 	}
 	
-	private double calculateTextPlacement() {
-		double height = this.getHeight();
-		if (height >= 30.0) {
-			return 24.0;
-		}
-		else {
-			return height * 0.4;
-		}
-	}
-	
 	private void putText() {
 		Text titleText = new Text(appointment.getTitle());
-		titleText.setStyle("-fx-font-family: 'Helvetica';");
-		titleText.setStyle("-fx-font-size: 15px;");
-		titleText.setStyle("-fx-font-style: italic");
-		titleText.setStyle("-fx-font-weight: lighter");
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");		
+		titleText.setFont(Font.font("Helvetica", FontWeight.THIN, 20));
 		this.getChildren().addAll(titleText);
-	}
-	
-	@Override
-	protected void layoutChildren() {
-		Text titleText = (Text)this.getChildren().get(0);
-		titleText.setLayoutX(10.0);
-		titleText.setLayoutY(calculateTextPlacement());
 	}
 }
--- Client/src/controllers/DayViewController.java
+++ Client/src/controllers/DayViewController.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import communication.requests.AppointmentRequest;
@@ -12,38 +14,104 @@ import communication.responses.AppointmentResponse;
 import calendar.AppointmentPane;
 import calendar.State;
 import models.Appointment;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 public class DayViewController implements Initializable{
 	
 	@FXML private AnchorPane dayPane;	
-	@FXML private Button getButton;
+	@FXML private Text dayLabel;
+	@FXML private Button prevDay;
+	@FXML private Button nextDay;
+	private String currentDay;
 	
+	Map<String, ArrayList<Appointment>> cachedAppointments = new HashMap<String, ArrayList<Appointment>>();
+	private final static DateTimeFormatter defaultFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+	private static ArrayList<Node> blankDayPane = new ArrayList<Node>();
+	
+
 	
 	public void initialize(URL location, ResourceBundle resources) {
 		
-		getButton.setOnAction(new EventHandler<ActionEvent>() {
+		//Necessary for concurrency, take a look at how the use of runLater can be avoided
+		Platform.runLater(new Runnable() {		
+			@Override public void run() {
+				
+				//Saves a blank dayPane as a template to use when changing dates
+				for (Node child : dayPane.getChildren()) {
+					blankDayPane.add(child);
+				}
+				currentDay = getToday();
+				dayLabel.setText(currentDay);	//Needs more beautiful text 
+				
+				//Caches the appointments
+				for (Appointment appointment : getAppointments()) {
+					if (cachedAppointments.get(appointment.getStartTime().substring(0, 10)) == null) {
+						cachedAppointments.put(appointment.getStartTime().substring(0, 10), new ArrayList<Appointment>());
+					}
+					cachedAppointments.get(appointment.getStartTime().substring(0, 10)).add(appointment);
+				}
+				addAppointmentsForNewDay();
+			}
+		});
+		
+		prevDay.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("handled button press");
-				ArrayList<Appointment> appointments = getAppointments();
-				for (Appointment appointment : appointments) {
-					AppointmentPane appPane = new AppointmentPane(appointment);
-					dayPane.getChildren().add(appPane);
-					AnchorPane.setTopAnchor(appPane, /*calculateAppointmentPlacement(appointment.getStartTime())*/ 600.0);
-					System.out.println("added appointment");
-				}
+				decrementDay();
+				dayPane.getChildren().retainAll(blankDayPane);
+				addAppointmentsForNewDay();
 			}
-		});	
+		});
+		
+		nextDay.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				incrementDay();
+				dayPane.getChildren().retainAll(blankDayPane);
+				addAppointmentsForNewDay();
+			}
+		});
+		
 	}
 	
-	public double calculateAppointmentPlacement(String startTime) {
+	private void addAppointmentsForNewDay() {
+		if (cachedAppointments.get(currentDay) != null) {
+			for (Appointment appointment : cachedAppointments.get(currentDay)) {
+				AppointmentPane pane = new AppointmentPane(appointment);
+				dayPane.getChildren().add(pane);
+				AnchorPane.setTopAnchor(pane, calculateAppointmentPlacement(appointment.getStartTime()));
+			}	
+		}
+	}
+	
+	private void incrementDay() {
+		LocalDate today = LocalDate.parse(currentDay, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		currentDay = today.plusDays(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		dayLabel.setText(currentDay);
+	}
+	
+	private void decrementDay() {
+		LocalDate today = LocalDate.parse(currentDay, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		currentDay = today.minusDays(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		dayLabel.setText(currentDay);
+	}
+	
+	private String getToday() {
+		System.out.println(LocalDateTime.now().format(defaultFormatter).substring(0,10));
+		return LocalDateTime.now().format(defaultFormatter).substring(0,10);
+	}
+		
+	private double calculateAppointmentPlacement(String startTime) {
 		String hourandminutes = startTime.substring(11);
 		System.out.println(hourandminutes);
 		String[] time = hourandminutes.split(":");
@@ -53,7 +121,7 @@ public class DayViewController implements Initializable{
 		return pixels;
 	}
 	
-	public ArrayList<Appointment> getAppointments() {
+	private ArrayList<Appointment> getAppointments() {
 		AppointmentRequest req = new AppointmentRequest();
 		req.addUsername(State.getUser().getUsername());
 		State.getConnectionController().sendTCP(req);
